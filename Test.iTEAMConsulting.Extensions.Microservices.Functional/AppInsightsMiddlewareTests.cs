6f64e6aa0fc30e690cc0b6ea9be6b8acae687392
Christopher Salinas <csalinas@iteamnm.com>
2022-09-27 08:46:12 -0600

    test: ensure ImplementationKey is set

---
+++ Test.iTEAMConsulting.Extensions.Microservices.Functional/AppInsightsMiddlewareTests.cs
@@ -0,0 +1,43 @@
+﻿using iTEAMConsulting.Extensions.Microservices;
+using Microsoft.ApplicationInsights;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Test.iTEAMConsulting.Extensions.Microservices.Functional
+{
+  public class AppInsightsMiddlewareTests
+  {
+
+    [Fact]
+    public async Task MiddlewareTest()
+    {
+      // Arrange
+      using var host = await new HostBuilder()
+        .ConfigureAppConfiguration(config => config
+          .AddJsonFile("appsettings.json"))
+        .ConfigureWebHost(builder => builder
+          .UseTestServer()
+          .ConfigureServices((context, services) =>
+          {
+            var options = context.Configuration
+              .GetSection("Microservice")
+              .Get<MicroserviceOptions>();
+            services.AddMicroservice(options);
+          })
+          .Configure(app => app.UseMicroservice()))
+        .StartAsync();
+
+      // Act
+      var server = host.GetTestServer();
+      var client = server.Services.GetRequiredService<TelemetryClient>();
+
+      // Assert
+      Assert.Equal("12345678-9012-3456-7890-123456789012", client.TelemetryConfiguration.InstrumentationKey);
+    }
+  }
+}
